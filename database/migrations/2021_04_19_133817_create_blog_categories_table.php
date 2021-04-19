182c8ac6e763b82308260d601605b222e18ee72c
Abmin <pavlo.koshubinskyi@oa.edu.ua>
2021-04-19 17:00:26 +0300

    lab 2

--- database/migrations/2021_04_19_133817_create_blog_categories_table.php
+++ database/migrations/2021_04_19_133817_create_blog_categories_table.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateBlogCategoriesTable extends Migration
 {
+
     /**
      * Run the migrations.
      *
@@ -13,9 +14,16 @@ class CreateBlogCategoriesTable extends Migration
      */
     public function up()
     {
+
         Schema::create('blog_categories', function (Blueprint $table) {
+
             $table->id();
+            $table->bigInteger('parent_id')->unsigned()->default('1');
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
